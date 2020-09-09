525052ecef3adc47c9f5f6326e86310f82e8fcc9
Praveen Kumar Pothapakula <70202759+pothapakulapraveen@users.noreply.github.com>
2020-09-09 13:40:06 +0200

    Add files via upload

---
+++ KERNEL/FIG1/TRASH/fig1_ker.R
@@ -0,0 +1,58 @@
+rm(list=ls())
+#library("Hmisc")
+#source("TE_function_kar.R")
+source("kernel_CMI.R")
+source("kernel_MI.R")
+#source(kernel_CMI.R"TE_mean.R")
+#source("TE_sd.R")
+#source("MI_function_kar.R")
+#source("CMI_function_kar.R")
+load("../../linear1.RData")
+library(doParallel)
+#cl<-makeCluster(detectCores())
+cl<-5
+registerDoParallel(cl)
+################# LONG time series ######################
+result4<-foreach(i = 1:10,.combine='rbind')%dopar%{
+#A<-TE(y[95000:100000],x[i,95000:100000])
+A<-CMI(y[94999:99999],x[i,95000:100000],x[i,94999:99999],1.5)
+B<-MI(y[94999:99999],x[i,95000:100000],1.5)
+C<-MI(x[i,94999:99999],x[i,95000:100000],1.5)
+#return(A)}
+return(c(A,B,C))}
+################### 1000 time units ############
+#result3<-foreach(i = 1:11,.combine='rbind')%dopar%{
+#A<-TE(y[99000:100000],x[i,99000:100000])
+#B<-MI(y[98999:99999],x[i,99000:100000])
+#C<-MI(x[94999:99999],x[i,95000:100000])
+#return(c(A,B,C))}
+#########################    500 time units      ###########
+#result2<-foreach(i = 1:11,.combine='rbind')%dopar%{
+#A<-TE(y[99500:100000],x[i,99500:100000])
+#B<-MI(y[99499:99999],x[i,99500:100000])
+##C<-MI(x[94999:99999],x[i,95000:100000])
+#return(c(A,B))}
+#########################   200 time units       #############
+#result1<-foreach(i = 1:11,.combine='rbind')%dopar%{
+#A<-TE(y[99800:100000],x[i,99800:100000])
+#B<-MI(y[99799:99999],x[i,99800:100000])
+#return(c(A,B))}
+####### 2 standard deviations ######
+NS<-result4[,1]-result4[,2]
+IXY<-result4[,1]+result4[,3]
+############################################################
+cp<-seq(0,1,0.1)
+plot(cp[1:9],IXY[1:9]*2,type="l",col="red",lwd=2.5,ylim=c(0,1),lty=1,pch=18,xlab="coupling coefficient",ylab="Information exchange (nats)",cex.lab=1.2,cex.axis=1.5,main="Kernel")
+#lines(cp,result3[,1],type="l",col="red",lwd=2.5)
+#lines(cp,result2[,1],type="l",col="red",lwd=2.5)
+#lines(cp,result1[,1],type="l",col="red",lwd=2.5)
+lines(cp[1:9],result4[1:9,2]*2,type="l",col="blue",lwd=2.5)
+lines(cp[1:9],result4[1:9,3]*2,type="l",col="green",lwd=2.5)
+lines(cp[1:9],NS[1:9]*2,type="l",col="black",lwd=2.5)
+#lines(cp,result2[,2],type="l",col="blue",lwd=2.5)
+#lines(cp,result1[,2],type="l",col="blue",lwd=2.5)
+lt<-c(1,2,3,4,5,1,2,3,4,5)
+pc<-c(18,23,20,21,22,18,23,20,21,22)
+colors<-c("red","red","red","red","red",rep("white",5),"blue","blue","blue","blue","blue")
+#legend("top",legend=c("n=5000","n=3000","n=1000","n=500","n=200",rep(" ",5),"n=5000","n=3000","n=1000","n=500","n=200"),ncol=3,col=colors,pch=pc,bty="n",cex=1.4,horiz=F,lty=lt,lwd=c(rep(1.4,5),rep(0,5),rep(1.4,5)),,text.width=c(0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.2))
+legend("topleft",legend=c("I(Xt;{Yt-1,Xt-1})","I(Xt;Yt-1)","I(Xt;Xt-1)","Net Synergy"),col=c("red","blue","green","black"),lty=1,lwd=1.4,bty="n",cex=1.4)
---
+++ KERNEL/FIG1/fig1_revision.R
@@ -0,0 +1,49 @@
+rm(list=ls())
+library("Hmisc")
+#source("CMI_function_kar.R")   ### Conditional Mutual Information Own!
+source("kernel_CMI.R")
+source("kernel_MI.R")
+source("kernel_MI_SD.R")
+source("kernel_CMI_SD.R")
+#source("MI_function_kar.R")        #### Mutual Information JDIT 
+#source("MI_SD_function_kar2.R")    ### Boot Strappig for MI using JDIT
+#source("CMI_SD_function_kar.R")    #### Boot Strapping for CMI using own algorithim
+load("../../linear1.RData")    #### Loading Linear Data
+library(doParallel)            ###   Parallel Library for Parallel execution using multiple processor
+cl<-5
+registerDoParallel(cl)
+################# Long time series ####################
+result4<-foreach(i = 1:9,.combine='rbind')%dopar%{
+A<-CMI(y[94999:99999],x[i,95000:100000],x[i,94999:99999],1.5)
+B<-MI(y[94999:99999],x[i,95000:100000],1.5)
+C<-MI(x[i,94999:99999],x[i,95000:100000],1.5)
+D<-CMI(x[i,94999:99999],x[i,95000:100000],y[94999:99999],1.5)
+return(c(A,B,C,D))}
+################### 1000 time units ############
+sd1<-foreach(i = 1:9,.combine='rbind')%dopar%{
+A<-MISD(y[95000:100000],x[i,95000:100000],1.5,100)    ## I(y_n-1;x_n)
+B<-MISD(x[i,95000:100000],x[i,95000:100000],1.5,100)   ## I(x_n-1; x_n)
+return(c(A,B))}
+#########################    500 time units      ###########
+sd2<-foreach(i = 1:9,.combine='rbind')%dopar%{
+A<-SDCMI(y[95000:100000],x[i,95000:100000],x[i,95000:100000],1.5,100)   ## I(y_n-1,x_n,/x_n-1)
+D<-SDCMI(x[i,95000:100000],x[i,95000:100000],y[95000:100000],1.5,100)   ## I(x_n-1,x_n/y_n-1))
+return(c(A,D))
+}
+####### Calculation of Information flow Metrics ######
+NS<-result4[,1]-result4[,2]    #### Net Synergy #####
+sdNS<-sqrt(sd2[1:9,1]^2 +  sd1[1:9,1]^2)   ####  Standard dviations of Net Synergy #####
+######
+IXY<-result4[,1]+result4[,3]              #####  IXY
+sdixy<- sqrt(sd2[1:9,1]^2 +  sd1[1:9,2]^2) ##### Standard deviations 
+IXX<-result4[,2] + result4[,4]
+############################################################
+cp<-seq(0,1,0.1)
+plot(cp[1:9],IXY[1:9],type="l",col="red",lwd=2.5,ylim=c(0,1),lty=1,pch=18,xlab="coupling coefficient",ylab="Information exchange (nats)",cex.lab=1.2,cex.axis=1.5,main="Kernel")
+errbar(cp[1:9],IXY[1:9],IXY[1:9]+2*sdixy[1:9],IXY[1:9]-2*sdixy[1:9],type="l",col='red',lwd=2.5,lty=1,pch=23,ylim=c(0,0.4),xlab="coupling coefficient",ylab="Transfer entropy",cex.lab=1.2,cex.axis=1.5,errbar.col='red',add=TRUE)
+##### MI SD ##
+errbar(cp[1:9],result4[1:9,2],result4[1:9,2]+2*sd1[1:9,1],result4[1:9,2]-2*sd1[1:9,1],type="l",col='blue',lwd=2.5,lty=1,pch=23,ylim=c(0,0.4),xlab="coupling coefficient",ylab="Transfer entropy",cex.lab=1.2,cex.axis=1.5,errbar.col='blue',add=TRUE)
+errbar(cp[1:9],result4[1:9,3],result4[1:9,3]+2*sd1[1:9,2],result4[1:9,3]-2*sd1[1:9,2],type="l",col='green',lwd=2.5,pch=23,add=TRUE,errbar.col='green')
+#############
+errbar(cp[1:9],NS[1:9],NS[1:9]+2*sdNS[1:9],NS[1:9]-2*sdNS[1:9],type="l",col='black',lwd=2.5,pch=23,add=TRUE,errbar.col='black')
+legend("topleft",legend=c("I(Xt;Yt-1,Xt-1)","I(Xt;Yt-1)","I(Xt;Xt-1)","NET SYNERGY"),col=c("red","blue","green","black"),lty=1,lwd=1.4,bty="n",cex=1.4)
